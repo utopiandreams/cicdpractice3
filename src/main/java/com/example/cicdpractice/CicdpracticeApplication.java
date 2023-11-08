a86286b4165ddae25e62161c40b9c3f4f0cd18af
utopiandreams <berserkppp@naver.com>
2023-11-08 19:24:31 +0900

    load project

---
+++ src/main/java/com/example/cicdpractice/CicdpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicdpractice/CicdpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
